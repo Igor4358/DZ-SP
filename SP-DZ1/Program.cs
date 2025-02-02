cf29a1923c0e6b48b78eb77075e63963d5fa7f03
Igor4358 <igor4358@yandex.ru>
2025-02-02 20:12:56 +0300

    Add files via upload

---
+++ SP-DZ1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP_DZ1
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+        static void Main(string[] args)
+        {
+            
+            string name = "Васильев Игорь";
+            string occupation = "Разработчик ПО";
+            string hobby = "Программирование, чтение, спорт";
+
+            // Отображение информации в нескольких MessageBox
+            MessageBox(IntPtr.Zero, $"Имя: {name}", "Информация о вас", 0);
+            MessageBox(IntPtr.Zero, $"Род занятий: {occupation}", "Информация о вас", 0);
+            MessageBox(IntPtr.Zero, $"Увлечения: {hobby}", "Информация о вас", 0);
+        }
+    }
+}
